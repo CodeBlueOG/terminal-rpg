5980443748126b6df5807d0a80e3c4e713981f60
Israel Quiroga <israelquiroga7@gmail.com>
2026-07-06 23:27:55 -0500

    Move travel and encounter generation into module

--- terminalRPG.js
+++ terminalRPG.js
@@ -8,26 +8,7 @@ const getRandomNumber = require("./utils/random");
 
 const shop = require("./systems/shop");
 const battle = require("./systems/battle");
-
-function generateEnemies(area) {
-    const enemyCount = getRandomNumber(area.minEnemies, area.maxEnemies);
-    const enemies = [];
-
-    for (let i = 0; i < enemyCount; i++) {
-        const randomEnemyName = area.possibleEnemies[
-            getRandomNumber(0, area.possibleEnemies.length - 1)
-        ];
-
-        const enemyTemplate = enemyTypes.find(enemy => enemy.name === randomEnemyName);
-
-        enemies.push({
-            ...enemyTemplate,
-            currentHealth: enemyTemplate.maxHealth
-        });
-    }
-
-    return enemies;
-};
+const travel = require("./systems/travel");
 
 async function startGame() {
     console.log("What is your character's name?");
@@ -42,53 +23,4 @@ console.log(
     await travel(player, areas);
 }
 
-async function travel(player, areas) {
-    while (player.health > 0) {
-        const unlockedAreas = areas.filter(
-            area => player.level >= area.requiredLevel
-        );
-
-        console.log("Where would you like to go?");
-
-        for (let i = 0; i < unlockedAreas.length; i++) {
-            const area = unlockedAreas[i];
-
-            console.log(
-                `${i + 1}. ${area.name} - Level ${area.requiredLevel}`
-            );
-        }
-
-        const shopOption = unlockedAreas.length + 1;
-        const quitOption = unlockedAreas.length + 2;
-
-        console.log(`${shopOption}. Shop`);
-        console.log(`${quitOption}. Quit Game`);
-
-        const choice = Number(await getInput());
-
-        if (choice === shopOption) {
-            await shop(player);
-            continue;
-        }
-
-        if (choice === quitOption) {
-            console.log("Thanks for playing!");
-            return;
-        }
-
-        const areaIndex = choice - 1;
-        const chosenArea = unlockedAreas[areaIndex];
-
-        if (!chosenArea) {
-            console.log("Invalid choice.");
-            continue;
-        }
-
-        console.log(`${player.name} enters ${chosenArea.name}.`);
-
-        const enemies = generateEnemies(chosenArea);
-
-        await battle(player, enemies);
-    }
-}
 startGame();
